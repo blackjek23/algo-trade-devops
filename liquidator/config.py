18560a71eb2acb05e386160d8899259de6f081d3
yakov shachar <yakov.shahar97@gmail.com>
2025-09-16 05:11:26 +0300

    adding module to close all positions

---
+++ liquidator/config.py
@@ -0,0 +1,10 @@
+# file: config.py
+
+# --- IBKR connection settings ---
+TWS_HOST = "127.0.0.1"  # or "host.docker.internal" if running inside Docker
+TWS_PORT = 7497  # 7497 live / 7496 paper / 4002 gateway paper
+CLIENT_ID = 1  # change if you want multiple connections
+TIMEOUT = 10  # seconds
+
+# --- Risk / Debug options ---
+DRY_RUN = False  # if True: do not send real orders, only print
