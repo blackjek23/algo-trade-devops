62c91c24e9af4e38987adace4d3713b6dd498f4b
yakov shachar <yakov.shahar97@gmail.com>
2025-10-08 20:14:19 +0300

    working telegram alerts

--- ledger/config.py
+++ ledger/config.py
@@ -2,6 +2,6 @@
 
 # --- IBKR connection settings ---
 TWS_HOST = "127.0.0.1"  # or "host.docker.internal" if running inside Docker
-TWS_PORT = 7497  # 7497 live / 7496 paper / 4002 gateway paper
+TWS_PORT = 4002  # 7497 live / 7496 paper / 4002 gateway paper
 CLIENT_ID = 4  # change if you want multiple connections
 TIMEOUT = 10  # seconds
